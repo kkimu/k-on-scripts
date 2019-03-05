131a2208bfc5f11da9e3ca5128be10479e1d84ca
kkimu <kimura.kzm@gmail.com>
2019-03-06 00:08:15 +0900

    Add timestamp field

--- mysql/init/init.sql
+++ mysql/init/init.sql
@@ -1,6 +1,8 @@
 create table if not exists artists (
 	id varchar(36) primary key,
 	name varchar(256) not null,
-	kanaPrefix varchar(128)
+	kanaPrefix varchar(128) not null,
+	createdAt timestamp default CURRENT_TIMESTAMP,
+	updatedAt timestamp default CURRENT_TIMESTAMP
 );
 
--- struct.go
+++ struct.go
@@ -1,7 +1,11 @@
 package main
 
+import "time"
+
 type Artist struct {
 	id         string
 	name       string
 	kanaPrefix string
+	createdAt time.Time
+	updatedAt time.Time
 }
